6ec6bc1c66bc6d7a0611ad86567c39124132ac93
Levi9111 <captainlevi9111@gmail.com>
2026-09-18 17:06:07 +0600

    feat: increase file creation limit to 30 in stress script and add df screenshot

--- scripts/03_stress_and_populate.sh
+++ scripts/03_stress_and_populate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 1 20); do
+for i in $(seq 1 30); do
     dd if=/dev/urandom of="/mnt/${SVC_NAME}_tmp/file_$i.dat" bs=1M count=10
     df -h "/mnt/${SVC_NAME}_tmp"
 done
